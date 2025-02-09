c9ffd6b8484aee83064bb64040ebf5ec6b771255
Nícolas Evangelista <65773163+nickevangelista@users.noreply.github.com>
2025-02-09 09:52:04 -0300

    Update CalculadoraJava.java

--- CalculadoraJava.java
+++ CalculadoraJava.java
@@ -1,5 +1,6 @@
 package Java;
 import java.util.Scanner;
+import java.util.InputMismatchException;
 import java.util.Locale;
 
 public class CalculadoraApp {
@@ -12,20 +13,26 @@ public class CalculadoraApp {
         String operacao;
         boolean continuar;
 
-        do {
-            System.out.println("Digite o valor um: ");
-            valorUm = scanner.nextDouble(); // receber o scanner, salva aqui o que foi lido
+        try{
+            do {
+                System.out.println("Digite o valor um: ");
+                valorUm = scanner.nextDouble(); // receber o scanner, salva aqui o que foi lido
 
-            System.out.println("Digite a operação (+, -, /, *): ");
-            operacao = scanner.next();
+                System.out.println("Digite a operação (+, -, /, *): ");
+                operacao = scanner.next();
 
-            System.out.println("Digite o valor dois: ");
-            valorDois = scanner.nextDouble();
+                System.out.println("Digite o valor dois: ");
+                valorDois = scanner.nextDouble();
 
-            System.out.println("Resultado: " + realizarCalculo(valorUm, valorDois, operacao));
-            
-            continuar = verificarNovaOperacao();
-        } while (continuar);
+                System.out.println("Resultado: " + realizarCalculo(valorUm, valorDois, operacao));
+                
+                continuar = verificarNovaOperacao();
+            } while (continuar);
+        } catch(InputMismatchException ex) {
+            System.out.println("Os valores para cálculo devem ser numéricos.");
+        }
+
+        
     }
 
     public static boolean verificarNovaOperacao(){
