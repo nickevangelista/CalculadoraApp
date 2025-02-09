4ca6ae8ed82844d11287bd7440580c193c677717
Nícolas Evangelista <65773163+nickevangelista@users.noreply.github.com>
2025-02-09 09:33:54 -0300

    Update CalculadoraJava.java
    Criei um laço de repetição do while para que o programa possa continuar perguntando valores como uma calculadora normal.

--- CalculadoraJava.java
+++ CalculadoraJava.java
@@ -1,5 +1,6 @@
 package Java;
 import java.util.Scanner;
+import java.util.Locale;
 
 public class CalculadoraApp {
     
@@ -9,19 +10,30 @@ public class CalculadoraApp {
         Double valorUm;
         Double valorDois;
         String operacao;
+        boolean continuar;
 
-        System.out.println("Digite o valor um: ");
-        valorUm = scanner.nextDouble(); // receber o scanner, salva aqui o que foi lido
+        do {
+            System.out.println("Digite o valor um: ");
+            valorUm = scanner.nextDouble(); // receber o scanner, salva aqui o que foi lido
 
-        System.out.println("Digite a operação (+, -, /, *): ");
-        operacao = scanner.next();
+            System.out.println("Digite a operação (+, -, /, *): ");
+            operacao = scanner.next();
 
-        System.out.println("Digite o valor dois: ");
-        valorDois = scanner.nextDouble();
+            System.out.println("Digite o valor dois: ");
+            valorDois = scanner.nextDouble();
 
-        System.out.println("Resultado: " + realizarCalculo(valorUm, valorDois, operacao));
+            System.out.println("Resultado: " + realizarCalculo(valorUm, valorDois, operacao));
+            
+            continuar = verificarNovaOperacao();
+        } while (continuar);
     }
 
+    public static boolean verificarNovaOperacao(){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Deseja realizar uma nova operação? (S/N): ");
+        return !sc.nextLine().toUpperCase(Locale.ROOT).equals("N"); // Se for igual a "N" interrompe o programa.
+    }
     public static Double realizarCalculo(Double valorUm, Double valorDois, String operacao) {
         Double respostaCalculo = 0.0;
         switch (operacao){
